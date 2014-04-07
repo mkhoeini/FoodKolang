9f72f67087403651bd4c0be6fd4497757cdea4ca
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2014-04-07 11:11:46 +0430

    Merged two views of food and index and some refactoring along the way

--- app/js/index.js
+++ app/js/index.js
@@ -1,14 +1,18 @@
 
-var gui = require('nw.gui');
-gui.Window.open('foods.html');
-
 angular.module('app', ['ui.bootstrap'])
-
 
 .controller('ctrl', function($scope) {
   var self = this;
 
   this.orderNumber = +localStorage.orderNumber || 1;
+
+  this.progName = localStorage.progName || 'کلنگ';
+
+  $scope.$watch('ctrl.progName', function (newValue) {
+    if (newValue) {
+      localStorage.progName = newValue;
+    }
+  });
 
   this.food = {
     name: 'سفارش',
@@ -30,6 +34,7 @@ angular.module('app', ['ui.bootstrap'])
   global.setFood = function (name, price) {
     self.food.name = name;
     self.food.price = price;
+    self.addFood();
     $scope.$digest();
   };
 
@@ -79,8 +84,78 @@ angular.module('app', ['ui.bootstrap'])
 .filter('persian', function () {
   return function (str) {
     var numbers = '۰۱۲۳۴۵۶۷۸۹';
-    return str.toString().replace(/\d/g, function(n) {
+    return (str || '').toString().replace(/\d/g, function(n) {
       return numbers[n];
     });
+  };
+})
+
+.directive('myEditable', function () {
+  function link(scope, elem, attrs) {
+    var span = elem.find('#mye-plain'),
+        input = elem.find('input');
+
+    span.click(function () {
+      span.hide();
+      input.show();
+      input.focus();
+    });
+
+    input.blur(function () {
+      span.show();
+      input.hide();
+    });
+
+    input.hide();
+
+    scope.$watch('myEditable', function (newValue) {
+      if((newValue || '').toString() === '') {
+        scope.myEditable = '-';
+      }
+    });
+  }
+
+  return {
+    restrict: 'A',
+    scope: {
+      iType: '@',
+      myEditable: '='
+    },
+    template:
+      '<span>' +
+        '<span id="mye-plain">' +
+          '{{myEditable | persian}}' +
+        '</span>' +
+        '<input class="non-print" type="{{iType}}" ng-model="myEditable">' +
+      '</span>',
+    link: link
+  };
+})
+
+.controller('foodsCntl', function ($scope) {
+  var self = this;
+  this.foods = JSON.parse(localStorage.foods || '[]');
+
+  this.setFood = function (name, price) {
+    global.setFood(name, price);
+  };
+
+  $scope.$watch('foods.foods', function (newValue) {
+    if (!newValue || !newValue.length) return;
+    localStorage.foods = JSON.stringify(newValue);
+  }, true);
+
+  this.moveUp = function(index) {
+    var f = self.foods.splice(index, 1)[0];
+    self.foods.splice(index - 1, 0, f);
+  };
+
+  this.moveDown = function(index) {
+    var f = self.foods.splice(index, 1)[0];
+    self.foods.splice(index + 1, 0, f);
+  };
+
+  this.newFood = function () {
+    self.foods.push({});
   };
 })
